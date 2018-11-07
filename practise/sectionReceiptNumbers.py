965d569117a4c6561cdb1629c1987d92e93f96b0
dragonmushu <akshay.ben.n@gmail.com>
2018-11-06 22:52:19 -0500

    Added more receipts, and extracted max value from receipt.

--- practise/sectionReceiptNumbers.py
+++ practise/sectionReceiptNumbers.py
@@ -30,7 +30,6 @@ def extractNumbersColumn(receiptFile):
     #horizontal morph_open (dilation followed by erosion)
     kernel = np.ones(HORIZONTAL_MORPH_KERNEL)
     processedImage = cv2.morphologyEx(threshImage, cv2.MORPH_OPEN, kernel)
-
     #contour using rotated rectangles
     _, contours, _ = cv2.findContours(processedImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     boxes = np.array([cv2.boxPoints(cv2.minAreaRect(c)) for c in contours])
@@ -69,33 +68,63 @@ def extractFloatingPoint(img, boundingBoxes):
 
     #create mask (inverted) such that it includes text and white space around
     #borders on pytesseract can cause noise
-    box = boundingBoxes[5]
-    mask = np.zeros((img.shape[0], img.shape[1]))
-    cv2.fillConvexPoly(mask, np.array(box, 'int32'), 255)
-    mask = np.array(mask, dtype='uint8')
-    mask= cv2.bitwise_not(mask)
-    isolatedText = cv2.bitwise_or(threshImage, mask)
-    #section of image
-    #find min y, max y, min x, max x
-    xMax = int(max([box[i][0] for i in range(0, 4)]))
-    yMax = int(max([box[i][1] for i in range(0, 4)]))
-    xMin = int(min([box[i][0] for i in range(0, 4)]))
-    yMin = int(min([box[i][1] for i in range(0, 4)]))
-    isolatedText = isolatedText[yMin:yMax, xMin:xMax]
-    #expand image
-    shape = isolatedText.shape
-    scaleFactor = shape[1]/EXPANDED_WIDTH
-    resizedText = cv2.resize(isolatedText, (int(shape[1]/scaleFactor), int(shape[0]/scaleFactor)))
-    #erode image to decrease holes
-    kernel = np.ones(EROSION_KERNEL)
-    textFilled = cv2.erode(resizedText, kernel)
-    #put image through tesseract and extract text
-    cv2.imwrite('temp.jpg', textFilled)
-    text = pytesseract.image_to_string(Image.open('temp.jpg'), config="--psm 13").strip()
-    print(text)
-    cv2.imshow('mask', textFilled)
+    allFloatingPoints = []
+    for box in boundingBoxes:
+        mask = np.zeros((img.shape[0], img.shape[1]))
+        cv2.fillConvexPoly(mask, np.array(box, 'int32'), 255)
+        mask = np.array(mask, dtype='uint8')
+        mask= cv2.bitwise_not(mask)
+        isolatedText = cv2.bitwise_or(threshImage, mask)
+        #section of image
+        #find min y, max y, min x, max x
+        xMax = int(max([box[i][0] for i in range(0, 4)]))
+        yMax = int(max([box[i][1] for i in range(0, 4)]))
+        xMin = int(min([box[i][0] for i in range(0, 4)]))
+        yMin = int(min([box[i][1] for i in range(0, 4)]))
+        isolatedText = isolatedText[yMin:yMax, xMin:xMax]
+        #expand image
+        shape = isolatedText.shape
+        scaleFactor = shape[1]/EXPANDED_WIDTH
+        resizedText = cv2.resize(isolatedText, (int(shape[1]/scaleFactor), int(shape[0]/scaleFactor)))
+        #erode image to decrease holes
+        kernel = np.ones(EROSION_KERNEL)
+        textFilled = cv2.erode(resizedText, kernel)
+        #TODO - affine the image so that the numbers are more straight
+        
+        #put image through tesseract and extract text
+        cv2.imwrite('temp.jpg', textFilled)
+        text = pytesseract.image_to_string(Image.open('temp.jpg'), config="--psm 13").strip()
+
+        #process alpha numerical parts of text to conform to #.#
+        #can be improved (need to analyze more receipts)
+        value = ''
+        for idx, c in enumerate(text):
+            if (c.isdigit()):
+                value += c
+            elif (c == '-' and len(value) == 0):
+                value += c
+            elif (c == '.' and value.find(c)==-1):
+                value += c
+            elif ((c == '_' or c == '-' or c == ',') and value.find('.') == -1):
+                value += '.'
+            elif (len(value) - value.find('.') - 1 == 2):
+                break
+            elif (len(value) > 0):
+                value = ''
+                break
+        if (len(value) != 0 and value.find('.')!=-1 and (len(value) - value.find('.') - 1 == 2)):
+            try:
+                allFloatingPoints.append(float(value.strip('""')))
+            except ValueError:
+                pass
+                
+    #cv2.imshow('mask', textFilled)
+    return allFloatingPoints
 
 
 
-img, threshImage, boxes = extractNumbersColumn('receipt6.jpg')
-extractFloatingPoint(threshImage, boxes)
+img, threshImage, boxes = extractNumbersColumn('receipt7.png')
+values = extractFloatingPoint(threshImage, boxes)
+print(values)
+cv2.imshow('image', threshImage)
+print('Total: ', max(values))
